fc3315cb0aa0260069939845a03473f08c926f75
atomicwrite <atomicbreathgodzilla@gmail.com>
2024-09-06 16:48:58 -0500

    Removed open api garbo

--- Benchie/Program.cs
+++ Benchie/Program.cs
@@ -7,7 +7,7 @@ var app = builder.Build();
 app.UseHttpsRedirection();
 
 app.MapGet("/helloworld", () => { return "Hello world"; })
-    .WithName("HelloWorld")
-    .WithOpenApi();
+    .WithName("HelloWorld");
+    //.WithOpenApi();
 
 app.Run();
