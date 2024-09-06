7a5e846c45a1adadb865651c4728baa410524959
atomicwrite <atomicbreathgodzilla@gmail.com>
2024-09-06 16:50:57 -0500

    Optimize using const

--- Benchie/Program.cs
+++ Benchie/Program.cs
@@ -5,8 +5,9 @@ var app = builder.Build();
 
  
 app.UseHttpsRedirection();
+const string helloWorld = "Hello world";
 
-app.MapGet("/helloworld", () => { return "Hello world"; })
+app.MapGet("/helloworld", () => helloWorld)
     .WithName("HelloWorld");
     //.WithOpenApi();
 
